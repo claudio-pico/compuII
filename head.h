09879f5d7041acb63837cc3c8c0efb506fb46f0f
CLAUDIOPICO <claudiopico@ubuntu.ubuntu-domain>
2015-10-19 15:03:29 -0300

    agrego make head servidorGyC

---
+++ head.h
@@ -0,0 +1,12 @@
+#ifndef _HEAD_H_
+#define _HEAD_H_
+
+#include <stdio.h>
+#include <netinet/in.h>
+#include <assert.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#endif
